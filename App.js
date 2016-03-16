dd33c2b2d506456d0d9e01f616968a60b99490c7
Uthra Krishnan <uakrishnan@gmail.com>
2016-03-15 19:25:25 -0700

    Set up word choice and display, and sound

--- App.js
+++ App.js
@@ -1,15 +1,55 @@
+    var synth = window.speechSynthesis;
+    var $cntrl = $('#playAudio');
+    var $currentImg = $('#currentImg');
+    var currentWord;
+    var missingLetter;
+    var displayWord;
+    var utterWord;
+    var $nextWord = $('#nextWord'); 
+    var currWordPool = wordpool.animals;
+    var optionsArray=[];
+
 $(document).ready(function(){
 
-var $cntrl = $('#playAudio');
+    // window.speechSynthesis.voice = 
 
 
 
+    $nextWord.on('click', function(){
+        // Select current Word
+        currentWord = currWordPool[Math.floor(Math.random() * currWordPool.length)];
+        // console.log(currentWord);
+        // Set image from current word
+        $currentImg.attr('src', currentWord.image);
+        //sets word to be spoken
+        utterWord = new SpeechSynthesisUtterance(currentWord.word);
 
-$cntrl.on('click', function(){
-    SpeechSynthesisUtterance.text = "Hello World";
-    SpeechSynthesisUtterance();
-});
+        // displays word with missing letter and call function
+        (function wordToDisplay(){
+            // randomly choose missing letter
+            missingLetter = _.sample(`${currentWord.word}`);
+            //Split string at missing letter and join with underscore
+            var displayWord = currentWord.word.split(missingLetter).join("__");
+            //append word with missing letter to div
+            $('#gameWord h2').text(displayWord);
+        })();
+        (function optionsToDisplay(){
+            optionsArray.push(missingLetter);
+            while (optionsArray.length<3){
+                var letter = String.fromCharCode(97 + Math.floor(Math.random() * 26));
+                if(optionsArray.indexOf(letter) === -1){
+                    optionsArray.push(letter);
+                }
+            } 
+            optionsArray = _.shuffle(optionsArray);
+            $('#options1').append(`<h3>${optionsArray[0]}</h3>`);
+            $('#options2').append(`<h3>${optionsArray[1]}</h3>`);
+            $('#options3').append(`<h3>${optionsArray[2]}</h3>`);
+        })();
+    });
 
-
+    $cntrl.on('click', function(){
+        synth.speak(utterWord);
+    });
 
 });

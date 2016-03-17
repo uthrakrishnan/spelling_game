d593ff0fb96abc289dad657d82f4ba688d76cf4a
Uthra Krishnan <uakrishnan@gmail.com>
2016-03-16 20:48:02 -0700

    added tamil

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
     var synth = window.speechSynthesis;
     var $cntrl = $('#playAudio');
     var $spanishcntrl = $('#playSpanishAudio');
+    var $chinesecntrl = $('#playChineseAudio');
+    var $tamilcntrl = $('#playTamilAudio');
     var $currentImg = $('#currentImg');
     var currentWord;
     var missingLetter;
     var displayWord;
     var utterWord;
+    var utterSpanishWord;
     var $nextWord = $('#nextWord'); 
     var currWordPool = wordpool.animals;
     var optionsArray=[];
@@ -15,10 +18,9 @@
 $(document).ready(function(){
 
     (function onStart(){
-        $answerOptions.show();
-        $('#spanish').hide();   
+        $answerOptions.show();   
         $('#wikipedia').hide(); 
-        $('#speaker').hide();  
+        $('#translations').hide();  
     })();  
 
     // window.speechSynthesis.voice = 
@@ -30,9 +32,8 @@ $(document).ready(function(){
         (function reset(){
             optionsArray=[];
             $answerOptions.show();
-            $('#spanish').hide();
+            $('#translations').hide();
             $('#wikipedia').hide();
-            $('#speaker').hide();
             $('#option1').removeClass('incorrect');
             $('#option2').removeClass('incorrect');
             $('#option3').removeClass('incorrect');
@@ -46,6 +47,9 @@ $(document).ready(function(){
         $currentImg.attr('src', currentWord.image);
         //sets word to be spoken
         utterWord = new SpeechSynthesisUtterance(currentWord.word);
+        utterWord.lang = "en-GB";
+        utterWord.name= "Google UK English Female";
+        utterWord.voiceURI = "Google UK English Female";
 
 
         // Get Wikipedia information
@@ -63,15 +67,50 @@ $(document).ready(function(){
             });
         })();
 
-        //Translate to spanishe
+        //Translate to spanish
         (function translateSpanish(){
             $.ajax({
                 url: `https://www.googleapis.com/language/translate/v2?key=AIzaSyD4G1Wqh3g-a3tnHO4340FPDqiOxqz5x-Y&q=${currentWord.word}&source=en&target=es`,
                 dataType: "json",
                 success: function (response){
-                    console.log(response);
-                    // $('#spanish h4').text(response);
-                    // $('')
+                    var spanishWord = response.data.translations[0].translatedText;
+                    $('#spanish').text(spanishWord);
+                    utterSpanishWord = new SpeechSynthesisUtterance(spanishWord);
+                    utterSpanishWord.lang = "es-US";
+                    utterSpanishWord.name= "Google español de Estados Unidos";
+                    utterSpanishWord.voiceURI = "Google español de Estados Unidos";
+                }
+            });
+        })();
+
+        //Translate to Tamil
+        (function translateTamil(){
+            $.ajax({
+                url: `https://www.googleapis.com/language/translate/v2?key=AIzaSyD4G1Wqh3g-a3tnHO4340FPDqiOxqz5x-Y&q=${currentWord.word}&source=en&target=ta`,
+                dataType: "json",
+                success: function (response){
+                    var tamilWord = response.data.translations[0].translatedText;
+                    $('#tamil').text(tamilWord);
+                    utterTamilWord = new SpeechSynthesisUtterance(tamilWord);
+                    utterTamilWord.lang = "id-ID";
+                    utterTamilWord.name= "Damayanti";
+                    utterTamilWord.voiceURI = "Damayanti";
+                }
+            });
+        })();
+
+        //Translate to Mandarin
+        (function translateMandarin(){
+            $.ajax({
+                url: `https://www.googleapis.com/language/translate/v2?key=AIzaSyD4G1Wqh3g-a3tnHO4340FPDqiOxqz5x-Y&q=${currentWord.word}&source=en&target=zh-CN`,
+                dataType: "json",
+                success: function (response){
+                    var chineseWord = response.data.translations[0].translatedText;
+                    $('#chinese').text(chineseWord);
+                    utterChineseWord = new SpeechSynthesisUtterance(chineseWord);
+                    utterChineseWord.lang = "zh-CN";
+                    utterChineseWord.name= "Google 普通话（中国大陆）";
+                    utterChineseWord.voiceURI = "Google 普通话（中国大陆）";
                 }
             });
         })();
@@ -79,10 +118,14 @@ $(document).ready(function(){
         // displays word with missing letter and call function
         (function wordToDisplay(){
             // randomly choose missing letter
-            missingLetter = _.sample(`${currentWord.word}`);
+            // missingLetter = _.sample(`${currentWord.word}`);
             //Split string at missing letter and join with underscore
-            displayWord = currentWord.word.split(missingLetter).join("__");
+            // displayWord = currentWord.word.split(missingLetter).join("__");
             //append word with missing letter to div
+            var wordIndex = Math.floor(Math.random()* currentWord.word.length);
+            var wordCopy = currentWord.word.slice();
+            displayWord = wordCopy.slice(0,wordIndex) + "_" + wordCopy.slice(wordIndex+1);
+            missingLetter = currentWord.word[wordIndex];
             $('#gameWord h2').text(displayWord);
         })();
         (function optionsToDisplay(){
@@ -109,9 +152,8 @@ $(document).ready(function(){
                 $('#gameWord h2').text(currentWord.word);
                 $('audio')[0].play();
                 $answerOptions.hide();
-                $('#spanish').show();
+                $('#translations').show();
                 $('#wikipedia').show();
-                $('#speaker').show();
                 console.log("Correct!");
             }
             else {
@@ -126,6 +168,15 @@ $(document).ready(function(){
         synth.speak(utterWord);
     });
 
+    $spanishcntrl.on('click', function(){
+        synth.speak(utterSpanishWord);
+    });
 
+    $tamilcntrl.on('click', function(){
+        synth.speak(utterTamilWord);
+    });
 
+    $chinesecntrl.on('click', function(){
+        synth.speak(utterChineseWord);
+    });
 });
